a65ec689630ae6c23aeeed898e43d3e9c20fe00b
W3xtar <122286713+W3xtar@users.noreply.github.com>
2023-11-18 12:58:27 +0100

    config.lua

--- config.lua
+++ config.lua
@@ -6,10 +6,10 @@ Config.Items = {
 	--Essen
 	["bread"] = {
 		type = "food",
-		prop = "prop_cs_burger_01",
-		status = 200000,
-		remove = true,
-		anim = {dict = 'mp_player_inteat@burger', name = 'mp_player_int_eat_burger_fp', settings = {8.0, -8, -1, 49, 0, 0, 0, 0}}
+		prop = "prop_cs_burger_01", -- Das item was in der animation verwendet wird https://forge.plebmasters.de/objects oder https://gta-objects.xyz
+		status = 200000,			-- wie Essen oder trinken in den Korper gehen soll was angezegt wird
+		remove = true,  			-- Wenn es aus dem Inventar verschwinden soll.
+		anim = {dict = 'mp_player_inteat@burger', name = 'mp_player_int_eat_burger_fp', settings = {8.0, -8, -1, 49, 0, 0, 0, 0}} -- hier kannst du dir die animationen aussuchen https://forge.plebmasters.de/animations/amb@bagels@male@walking@@static?ped=A_F_Y_Beach_01
 	},
 	["sandwich"] = {
 		type = "food",
